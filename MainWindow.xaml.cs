735d107b9a04fa699e053a76faebc1991310424b
Mitchelded <tretyakovanton0000@gmail.com>
2024-06-13 13:25:02 +0300

    add depatrmnet group

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -42,9 +42,17 @@ namespace PP05Tretyakov
 
             switch (tableName)
             {
-                case "Employee": EmployeeDG.ItemsSource = _db.Employee.Local.ToBindingList();
+                case "Employee":
+                    EmployeeDG.ItemsSource = _db.Employee.Local.ToBindingList();
                     DGComboBoxColumnDepartmentName.ItemsSource = _db.Department.ToList();
                     DGComboBoxColumnContractNumber.ItemsSource = _db.Contract.ToList();
+                    foreach (var item in _db.Department.ToList())
+                    {
+                        if(!CBDepartment.Items.Equals(item))
+                        {
+                            CBDepartment.Items.Add(item.Name);
+                        }
+                    }
                     break;
                 case "Contract": ContractDG.ItemsSource = _db.Contract.Local.ToBindingList(); break;
                 case "Department": DepartmentDG.ItemsSource = _db.Department.Local.ToBindingList(); break;
@@ -56,11 +64,20 @@ namespace PP05Tretyakov
         {
             _db.SaveChanges();
         }
-        
+
 
         private void MainWindow_OnClosing(object sender, CancelEventArgs e)
         {
             _db.Dispose();
+        }
+
+        private void CBDepartment_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var t = CBDepartment.SelectedValue.ToString();
+            if(t.Contains("All"))
+                EmployeeDG.ItemsSource = _db.Employee.Local.ToBindingList();
+            else
+                EmployeeDG.ItemsSource = _db.Employee.Local.Where(x => x.Department_Name == t).ToList();
         }
     }
 }
